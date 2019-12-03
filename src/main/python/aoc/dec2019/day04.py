6d0d84ea4b41b25af038c937f47f799cf3edd463
Dan Hasday <dhasday@gmail.com>
2019-12-03 01:18:34 -0500

    Prepping for day 4 a bit early

---
+++ src/main/python/aoc/dec2019/day04.py
@@ -0,0 +1,12 @@
+from aoc.common.day_solver import DaySolver
+
+
+class Day04Solver(DaySolver):
+    year = 2019
+    day = 4
+
+    def solve_puzzle_one(self):
+        return None
+
+    def solve_puzzle_two(self):
+        return None
