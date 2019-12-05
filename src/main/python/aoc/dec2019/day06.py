497dfa3b950349a0b0306a1aedca0b016f545cc3
Dan Hasday <dhasday@gmail.com>
2019-12-05 03:21:58 -0500

    Prepping for day 6 2019

---
+++ src/main/python/aoc/dec2019/day06.py
@@ -0,0 +1,12 @@
+from aoc.common.day_solver import DaySolver
+
+
+class Day06Solver(DaySolver):
+    year = 2019
+    day = 6
+
+    def solve_puzzle_one(self):
+        return None
+
+    def solve_puzzle_two(self):
+        return None
