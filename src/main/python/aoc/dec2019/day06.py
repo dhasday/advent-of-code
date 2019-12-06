c78e962ca739eea49b683be0e3917cd4ddd011fc
Dan Hasday <dhasday@gmail.com>
2019-12-06 00:52:34 -0500

    Solution for Day 6
    Got to reuse DijkstraSearch which saved a bunch of time

--- src/main/python/aoc/dec2019/day06.py
+++ src/main/python/aoc/dec2019/day06.py
@@ -1,4 +1,5 @@
 from aoc.common.day_solver import DaySolver
+from aoc.common.dijkstra_search import DijkstraSearch
 
 
 class Day06Solver(DaySolver):
@@ -6,7 +7,47 @@ class Day06Solver(DaySolver):
     day = 6
 
     def solve_puzzle_one(self):
-        return None
+        orbits = self._load_orbits(symmetric=False)
+
+        return self._count_orbits(orbits, 'COM', 0)
 
     def solve_puzzle_two(self):
-        return None
+        orbits = self._load_orbits(symmetric=True)
+
+        min_distance = self._find_min_distance(orbits, 'YOU', 'SAN')
+
+        return min_distance - 2
+
+    def _load_orbits(self, symmetric):
+        lines = self._load_all_input_lines()
+
+        orbits = dict()
+        for line in lines:
+            planets = line.split(')')
+            inner = planets[0]
+            outer = planets[1]
+
+            if inner not in orbits:
+                orbits[inner] = set()
+            orbits[inner].add(outer)
+
+            if symmetric:
+                if outer not in orbits:
+                    orbits[outer] = set()
+                orbits[outer].add(inner)
+
+        return orbits
+
+    def _count_orbits(self, orbits, planet, distance):
+        count = distance
+
+        if planet in orbits:
+            for p in orbits[planet]:
+                count += self._count_orbits(orbits, p, distance + 1)
+
+        return count
+
+    def _find_min_distance(self, orbits, start, end):
+        search = DijkstraSearch(lambda p: [(v, 1) for v in orbits[p]])
+        path, distance = search.find_shortest_path(start, end)
+        return distance
