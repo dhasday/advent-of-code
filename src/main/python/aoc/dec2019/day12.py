7c804bc6e6d9925df8abe528f0de114ba32ee2c1
Dan Hasday <dhasday@gmail.com>
2019-12-12 18:52:27 -0500

    Updates to today's puzzles

--- src/main/python/aoc/dec2019/day12.py
+++ src/main/python/aoc/dec2019/day12.py
@@ -12,72 +12,83 @@ class Day12Solver(DaySolver):
     year = 2019
     day = 12
 
+    class Moon:
+        def __init__(self, line):
+            self.position = [int(v) for v in ALL_NUMBERS_REGEX.findall(line)]
+            self.velocity = [0, 0, 0]
+
+        def gravitate(self, moon):
+            self._gravitate(moon, 0)
+            self._gravitate(moon, 1)
+            self._gravitate(moon, 2)
+
+        def _gravitate(self, moon, idx):
+            if self.position[idx] < moon.position[idx]:
+                self.velocity[idx] += 1
+                moon.velocity[idx] -= 1
+            elif self.position[idx] > moon.position[idx]:
+                self.velocity[idx] -= 1
+                moon.velocity[idx] += 1
+
+        def move(self):
+            self.position[0] += self.velocity[0]
+            self.position[1] += self.velocity[1]
+            self.position[2] += self.velocity[2]
+
+        def format_pair(self, idx):
+            return '{},{}'.format(self.position[idx], self.velocity[idx])
+
+        @property
+        def total_energy(self):
+            pot = sum(abs(p) for p in self.position)
+            kin = sum(abs(v) for v in self.velocity)
+            return pot * kin
+
     def solve_puzzle_one(self):
-        pos, vel = self._init_input()
+        lines = self._load_all_input_lines()
+        moons = list(self.Moon(line) for line in lines)
 
         for i in range(1000):
-            self._run_step(pos, vel)
+            self._run_step(moons)
 
-        return self._total_energy(pos, vel)
+        return sum(m.total_energy for m in moons)
 
     def solve_puzzle_two(self):
-        pos, vel = self._init_input()
+        lines = self._load_all_input_lines()
+        moons = list(self.Moon(line) for line in lines)
 
-        seen = [set(), set(), set()]
+        targets = [' '.join(m.format_pair(i) for m in moons) for i in range(3)]
         cycles = [None, None, None]
-        remaining = 3
 
-        i = 0
-        while remaining:
-            for j in range(3):
-                if cycles[j] is None:
-                    r = ','.join(str(v) for v in (pos[j::3] + vel[j::3]))
-                    if r in seen[j]:
-                        cycles[j] = i
-                        remaining -= 1
-                    else:
-                        seen[j].add(r)
+        ctr = 0
+        while not all(cycles) or ctr < 3000:
+            self._run_step(moons)
+            ctr += 1
 
-            self._run_step(pos, vel)
-            i += 1
+            curs = ['', '', '']
+            for m in moons:
+                curs[0] += m.format_pair(0) + ' '
+                curs[1] += m.format_pair(1) + ' '
+                curs[2] += m.format_pair(2) + ' '
+
+            if cycles[0] is None and targets[0] == curs[0][:-1]:
+                cycles[0] = ctr
+            if cycles[1] is None and targets[1] == curs[1][:-1]:
+                cycles[1] = ctr
+            if cycles[2] is None and targets[2] == curs[2][:-1]:
+                cycles[2] = ctr
 
         return self._lcm(cycles)
 
-    def _init_input(self):
-        lines = self._load_all_input_lines()
+    def _run_step(self, moons):
+        num = len(moons)
 
-        pos = list(int(v) for v in ALL_NUMBERS_REGEX.findall(''.join(lines)))
-        vel = [0] * len(pos)
-        return pos, vel
+        for i in range(num - 1):
+            for j in range(i + 1, num):
+                moons[i].gravitate(moons[j])
 
-    def _run_step(self, pos, vel):
-        for i in range(3):
-            for j in range(i + 1, 4):
-                for x in range(3):
-                    idx_1 = i * 3 + x
-                    idx_2 = j * 3 + x
-                    if pos[idx_1] < pos[idx_2]:
-                        vel[idx_1] += 1
-                        vel[idx_2] -= 1
-                    elif pos[idx_1] > pos[idx_2]:
-                        vel[idx_1] -= 1
-                        vel[idx_2] += 1
-
-        for i in range(len(pos)):
-            pos[i] += vel[i]
-
-    def _total_energy(self, pos, vel):
-        total = 0
-
-        for i in range(4):
-            pot = 0
-            kin = 0
-            for j in range(3):
-                pot += abs(pos[i * 3 + j])
-                kin += abs(vel[i * 3 + j])
-            total += pot * kin
-
-        return total
+        for m in moons:
+            m.move()
 
     def _lcm(self, values):
         lcm = values[0]
