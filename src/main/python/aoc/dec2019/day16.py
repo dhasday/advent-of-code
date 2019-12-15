b3d2712acc90707c3dc866faa5057e9b5d12780d
Dan Hasday <dhasday@gmail.com>
2019-12-15 01:27:19 -0500

    Prep for day 16

---
+++ src/main/python/aoc/dec2019/day16.py
@@ -0,0 +1,21 @@
+import math
+import re
+from collections import deque
+
+from aoc.common.day_solver import DaySolver
+from aoc.common.dijkstra_search import DijkstraSearch
+from aoc.dec2019.common.intcode_processor import IntcodeProcessor
+
+ALL_NUMBERS_REGEX = re.compile(r'-?\d+')
+
+
+class Day16Solver(DaySolver):
+    year = 2019
+    day = 16
+
+    def solve_puzzle_one(self):
+        return None
+
+    def solve_puzzle_two(self):
+        return None
+
