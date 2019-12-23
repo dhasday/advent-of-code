ff8ff59b92dab217b30dc03bac4b3133f16af09b
Dan Hasday <dhasday@gmail.com>
2019-12-23 01:12:05 -0500

    A little bit of clean-up

--- src/main/python/aoc/dec2019/day23.py
+++ src/main/python/aoc/dec2019/day23.py
@@ -3,7 +3,6 @@ from collections import deque
 from aoc.common.day_solver import DaySolver
 from aoc.dec2019.common.intcode_processor import IntcodeProcessor
 
-
 NUM_NODES = 50
 
 
@@ -11,36 +10,34 @@ class Day23Solver(DaySolver):
     year = 2019
     day = 23
 
-    def solve_puzzle_one(self):
+    def solve_puzzles(self):
         processors, queued_values = self._init_processors()
 
-        output_values = [[] for _ in range(NUM_NODES)]
-        while True:
-            for i in range(NUM_NODES):
-                p = processors[i]
-                p.execute_current_step()
-                if p.last_opcode == 4:
-                    out = output_values[i]
-                    if len(out) < 2:
-                        out.append(p.last_output)
-                    else:
-                        addr = out[0]
-                        x = out[1]
-                        y = p.last_output
+        first_nat_y, first_repeat_nat_y = self._run_processors(processors, queued_values)
 
-                        if 0 <= addr <= 49:
-                            queued_values[addr].append(x)
-                            queued_values[addr].append(y)
-                        elif addr == 255:
-                            return y
+        return first_nat_y, first_repeat_nat_y
 
-                        output_values[i] = []
+    def _init_processors(self):
+        def input_func(idx):
+            buffered = queued_values[idx]
+            return lambda: buffered.popleft() if len(buffered) else -1
 
-    def solve_puzzle_two(self):
-        processors, queued_values = self._init_processors()
+        line = self.load_only_input_line()
 
+        processors = []
+        queued_values = [deque() for _ in range(NUM_NODES)]
+        for i in range(NUM_NODES):
+            processor = IntcodeProcessor(line, input_value=i)
+            processor.execute_current_step()
+            processor.input_func = input_func(i)
+            processors.append(processor)
+
+        return processors, queued_values
+
+    def _run_processors(self, processors, queued_values):
         output_values = [[] for _ in range(NUM_NODES)]
         idling = [False] * NUM_NODES
+        first_nat_y = None
         nat_output = None
         prev_output = None
         while True:
@@ -69,35 +66,19 @@ class Day23Solver(DaySolver):
                             queued_values[addr].append(y)
                         elif addr == 255:
                             nat_output = x, y
+                            if not first_nat_y:
+                                first_nat_y = y
 
                         output_values[i] = []
 
             if nat_output is not None and all(idling):
                 if prev_output == nat_output:
-                    return nat_output[1]
+                    return first_nat_y, nat_output[1]
 
-                print(nat_output)
                 queued_values[0].append(nat_output[0])
                 queued_values[0].append(nat_output[1])
                 prev_output = nat_output
                 idling = [False] * NUM_NODES
-
-    def _init_processors(self):
-        def input_func(idx):
-            buffered = queued_values[idx]
-            return lambda: buffered.popleft() if len(buffered) else -1
-
-        line = self.load_only_input_line()
-
-        processors = []
-        queued_values = [deque() for _ in range(NUM_NODES)]
-        for i in range(NUM_NODES):
-            processor = IntcodeProcessor(line, input_value=i)
-            processor.execute_current_step()
-            processor.input_func = input_func(i)
-            processors.append(processor)
-
-        return processors, queued_values
 
 
 if __name__ == '__main__':
