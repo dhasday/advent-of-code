56d4c0ac9d49a0935ea286972796aa6f6dbbf113
Dan Hasday <dhasday@gmail.com>
2019-12-24 02:43:32 -0500

    Fun puzzles today, if only i could keep my rows and cols straight

--- src/main/python/aoc/dec2019/day24.py
+++ src/main/python/aoc/dec2019/day24.py
@@ -1,15 +1,6 @@
-import math
-import re
-from collections import deque, defaultdict
-from functools import reduce
-from itertools import islice, cycle
-
-from aoc.common.a_star_search import AStarSearch
-from aoc.common.breadth_first_search import BreadthFirstSearch
 from aoc.common.day_solver import DaySolver
-from aoc.common.dijkstra_search import DijkstraSearch
-from aoc.common.helpers import ALL_NUMBERS_REGEX, STANDARD_DIRECTIONS
-from aoc.dec2019.common.intcode_processor import IntcodeProcessor
+
+EMPTY_LAYER = '.' * 25
 
 
 class Day24Solver(DaySolver):
@@ -17,10 +8,123 @@ class Day24Solver(DaySolver):
     day = 24
 
     def solve_puzzle_one(self):
-        return None
+        lines = self.load_all_input_lines()
+
+        state = ''.join(lines)
+
+        seen = set()
+        while state not in seen:
+            seen.add(state)
+            next_state = ''
+            for offset in range(25):
+                cur_val = state[offset]
+                row = offset // 5
+                col = offset % 5
+
+                adj_bugs = 0
+
+                if col > 0:
+                    adj_bugs += 1 if state[offset - 1] == '#' else 0
+                if col < 4:
+                    adj_bugs += 1 if state[offset + 1] == '#' else 0
+                if row > 0:
+                    adj_bugs += 1 if state[offset - 5] == '#' else 0
+                if row < 4:
+                    adj_bugs += 1 if state[offset + 5] == '#' else 0
+
+                if cur_val == '#':
+                    next_state += '#' if adj_bugs == 1 else '.'
+                else:
+                    next_state += '#' if adj_bugs in [1, 2] else '.'
+
+            state = next_state
+
+        rating = 0
+        points = 1
+        for val in state:
+            if val == '#':
+                rating += points
+            points *= 2
+
+        return rating
 
     def solve_puzzle_two(self):
-        return None
+        state = [''.join(self.load_all_input_lines())]
+
+        for _ in range(200):
+            # Pad with extra layer on each end for expansion
+            test_state = [EMPTY_LAYER] + state + [EMPTY_LAYER]
+            num_layers = len(test_state)
+
+            next_state = []
+            for i in range(num_layers):
+                next_layer = ''
+                for idx in range(25):
+                    next_layer += self._get_next_value_recursive(
+                        idx // 5,
+                        idx % 5,
+                        test_state[i - 1] if i > 0 else None,
+                        test_state[i],
+                        test_state[i + 1] if i < num_layers - 1 else None,
+                    )
+                next_state.append(next_layer)
+
+            # Prune empty layers
+            while next_state[0] == EMPTY_LAYER:
+                next_state = next_state[1:]
+            while next_state[-1] == EMPTY_LAYER:
+                next_state = next_state[:-1]
+
+            state = next_state
+
+        return len(''.join(state).replace('.', ''))
+
+    def _get_next_value_recursive(self, row, col, layer_up, cur_layer, layer_down):
+        if col == 2 and row == 2:
+            return '.'
+
+        adj_bugs = 0
+        offset = row * 5 + col
+        cur_val = cur_layer[offset]
+
+        if col > 0:
+            adj_bugs += 1 if cur_layer[offset - 1] == '#' else 0
+        if col < 4:
+            adj_bugs += 1 if cur_layer[offset + 1] == '#' else 0
+        if row > 0:
+            adj_bugs += 1 if cur_layer[offset - 5] == '#' else 0
+        if row < 4:
+            adj_bugs += 1 if cur_layer[offset + 5] == '#' else 0
+
+        if layer_up is not None:
+            if col == 0:
+                adj_bugs += 1 if layer_up[11] == '#' else 0  # 2, 1
+            elif col == 4:
+                adj_bugs += 1 if layer_up[13] == '#' else 0  # 2, 3
+
+            if row == 0:
+                adj_bugs += 1 if layer_up[7] == '#' else 0  # 1, 2
+            elif row == 4:
+                adj_bugs += 1 if layer_up[17] == '#' else 0  # 3, 2
+
+        if layer_down is not None:
+            if row == 1 and col == 2:
+                for i in range(5):
+                    adj_bugs += 1 if layer_down[i] == '#' else 0
+            elif row == 2 and col == 1:
+                for i in range(5):
+                    adj_bugs += 1 if layer_down[i * 5] == '#' else 0
+            elif row == 2 and col == 3:
+                for i in range(5):
+                    adj_bugs += 1 if layer_down[i * 5 + 4] == '#' else 0
+            elif row == 3 and col == 2:
+                for i in range(5):
+                    adj_bugs += 1 if layer_down[20 + i] == '#' else 0
+
+        if cur_val == '#':
+            return '#' if adj_bugs == 1 else '.'
+        else:
+            return '#' if adj_bugs in [1, 2] else '.'
 
 
 if __name__ == '__main__':
---
+++ src/main/python/aoc/dec2019/day25.py
@@ -0,0 +1,28 @@
+import math
+import re
+from collections import deque, defaultdict
+from functools import reduce
+from itertools import islice, cycle
+
+from aoc.common.a_star_search import AStarSearch
+from aoc.common.breadth_first_search import BreadthFirstSearch
+from aoc.common.day_solver import DaySolver
+from aoc.common.dijkstra_search import DijkstraSearch
+from aoc.common.helpers import ALL_NUMBERS_REGEX, STANDARD_DIRECTIONS
+from aoc.dec2019.common.intcode_processor import IntcodeProcessor
+
+
+class Day25Solver(DaySolver):
+    year = 2019
+    day = 25
+
+    def solve_puzzle_one(self):
+        return None
+
+    def solve_puzzle_two(self):
+        return 'ALL DONE!'
+
+
+if __name__ == '__main__':
+    Day25Solver().print_results()
+
