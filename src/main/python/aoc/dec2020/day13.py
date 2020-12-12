3f9998227e3aade279a23aea23e9ecee99072b71
Dan Hasday <dhasday@gmail.com>
2020-12-12 01:06:31 -0500

    Prep for tomorrow

---
+++ src/main/python/aoc/dec2020/day13.py
@@ -0,0 +1,16 @@
+import re
+
+from aoc.common.day_solver import DaySolver
+
+INPUT_REGEX = re.compile(r'')
+
+
+class Day13Solver(DaySolver):
+    year = 2020
+    day = 13
+
+    def solve_puzzle_one(self):
+        return 'TODO'
+
+    def solve_puzzle_two(self):
+        return 'TODO'
