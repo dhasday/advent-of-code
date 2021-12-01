5795ad9d3b0aa8e875c0b04fb48aefec78d5ec21
Dan Hasday <dhasday@gmail.com>
2021-12-01 00:14:30 -0500

    And we're off, day 1 complete

--- src/main/python/aoc/dec2021/day01.py
+++ src/main/python/aoc/dec2021/day01.py
@@ -8,9 +8,30 @@ class Day01Solver(DaySolver):
     def solve_puzzle_one(self):
         lines = self.load_all_input_lines()
 
-        return 'TODO'
+        prev_val = None
+        count = 0
+        for line in lines:
+            val = int(line)
+            if prev_val and val > prev_val:
+                count += 1
+            prev_val = val
+
+        return count
 
     def solve_puzzle_two(self):
-        lines = self.load_all_input_lines()
+        values = [int(v) for v in self.load_all_input_lines()]
 
-        return 'TODO'
+        count = 0
+
+        prev_2 = values[1]
+        prev_3 = values[2]
+        prev_total = values[0] + prev_2 + prev_2
+        for i in values[3:]:
+            new_total = prev_2 + prev_3 + i
+            if new_total > prev_total:
+                count += 1
+            prev_2 = prev_3
+            prev_3 = i
+            prev_total = new_total
+
+        return count
