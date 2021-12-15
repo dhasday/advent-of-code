121f9c55f1cac4e0e635c83dea39083d0b37a13b
Dan Hasday <dhasday@gmail.com>
2021-12-15 00:45:09 -0500

    Prepping for day 16

---
+++ src/main/python/aoc/dec2021/day16.py
@@ -0,0 +1,20 @@
+from aoc.common.a_star_search import AStarSearch
+from aoc.common.day_solver import DaySolver
+from aoc.common.dijkstra_search import DijkstraSearch
+from aoc.common.helpers import STANDARD_DIRECTIONS
+
+
+class Day16Solver(DaySolver):
+    year = 2021
+    day = 16
+
+    def solve_puzzle_one(self):
+        lines = self.load_all_input_lines()
+
+        return 'TODO'
+
+    def solve_puzzle_two(self):
+        return 'TODO'
+
+
+Day16Solver().print_results()
