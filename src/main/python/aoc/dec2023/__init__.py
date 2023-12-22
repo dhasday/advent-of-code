c0010eab766417a55ec738fa2b351d25612e5382
Dan Hasday <dhasday@gmail.com>
2023-12-22 10:07:03 -0500

    Solution for Day 22 2023
     - Also some additional prep to make it easier for the rest of the days when on a slower laptop

--- src/main/python/aoc/dec2023/__init__.py
+++ src/main/python/aoc/dec2023/__init__.py
@@ -23,7 +23,7 @@ from .day19 import Day19Solver
 from .day20 import Day20Solver
 
 from .day21 import Day21Solver
-# from .day22 import Day22Solver
+from .day22 import Day22Solver
 # from .day23 import Day23Solver
 # from .day24 import Day24Solver
 # from .day25 import Day25Solver
--- src/main/python/aoc/dec2023/day24.py
+++ src/main/python/aoc/dec2023/day24.py
@@ -10,17 +10,17 @@ class Day24Solver(DaySolver):
 
     def solve_puzzle_one(self):
         # line = self.load_only_input_line(example=True)
-        line = self.load_only_input_line()
+        # line = self.load_only_input_line()
         # lines = self.load_all_input_lines(example=True)
-        lines = self.load_all_input_lines()
+        # lines = self.load_all_input_lines()
 
         return 'TODO'
 
     def solve_puzzle_two(self):
         # line = self.load_only_input_line(example=True)
-        line = self.load_only_input_line()
+        # line = self.load_only_input_line()
         # lines = self.load_all_input_lines(example=True)
-        lines = self.load_all_input_lines()
+        # lines = self.load_all_input_lines()
 
         return 'TODO'
 
