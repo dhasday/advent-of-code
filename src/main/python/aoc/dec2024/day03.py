1ce02029376a635a986f3b9fea965362f132d8a0
Dan Hasday <dhasday@gmail.com>
2024-12-03 07:34:44 -0500

    Solution for day 3

--- src/main/python/aoc/dec2024/day03.py
+++ src/main/python/aoc/dec2024/day03.py
@@ -1,28 +1,32 @@
+import re
+
 from aoc.common.day_solver import DaySolver
 
+
+INSTRUCTION_REGEX = re.compile('(do|don\'t)\(\)|mul\((\d{1,3}),(\d{1,3})\)')
 
 class Day03Solver(DaySolver):
     year = 2024
     day = 3
 
-    def setup(self):
-        pass
-
-    def solve_puzzle_one(self):
-        # line = self.load_only_input_line(example=True)
-        line = self.load_only_input_line()
-        # lines = self.load_all_input_lines(example=True)
+    def solve_puzzles(self):
         lines = self.load_all_input_lines()
 
-        return 'TODO'
+        mul_enabled = True
+        total_p1 = 0
+        total_p2 = 0
+        for line in lines:
+            matches = INSTRUCTION_REGEX.findall(line)
+            for match in matches:
+                if match[0] == 'do':
+                    mul_enabled = True
+                elif match[0] == 'don\'t':
+                    mul_enabled = False
+                else:
+                    mul_result = int(match[1]) * int(match[2])
 
-    def solve_puzzle_two(self):
-        # line = self.load_only_input_line(example=True)
-        line = self.load_only_input_line()
-        # lines = self.load_all_input_lines(example=True)
-        lines = self.load_all_input_lines()
+                    total_p1 += mul_result
+                    if mul_enabled:
+                        total_p2 += mul_result
 
-        return 'TODO'
-
-
-Day03Solver().print_results()
+        return total_p1, total_p2
