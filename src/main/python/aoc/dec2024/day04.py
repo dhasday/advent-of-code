23604982050c84d52fba2e1c9bad4a038f2be297
Dan Hasday <dhasday@gmail.com>
2024-12-04 00:50:33 -0500

    Solution for 2024 Day 4 and prep for Day 5

--- src/main/python/aoc/dec2024/day04.py
+++ src/main/python/aoc/dec2024/day04.py
@@ -1,28 +1,56 @@
 from aoc.common.day_solver import DaySolver
+from aoc.common.helpers import STANDARD_DIRECTIONAL_OFFSETS
 
 
 class Day04Solver(DaySolver):
     year = 2024
     day = 4
 
-    def setup(self):
-        pass
+    TARGET_P2 = {'M', 'S'}
 
-    def solve_puzzle_one(self):
-        # line = self.load_only_input_line(example=True)
-        line = self.load_only_input_line()
-        # lines = self.load_all_input_lines(example=True)
+    def solve_puzzles(self):
         lines = self.load_all_input_lines()
 
-        return 'TODO'
+        puzzle = {}
+        x_pos = []
+        a_pos = []
+        for y, line in enumerate(lines):
+            for x, val in enumerate(line):
+                pos = x, y
+                if val in 'MAS':
+                    puzzle[pos] = val
+                    if val == 'A':
+                        a_pos.append(pos)
+                elif val == 'X':
+                    x_pos.append(pos)
 
-    def solve_puzzle_two(self):
-        # line = self.load_only_input_line(example=True)
-        line = self.load_only_input_line()
-        # lines = self.load_all_input_lines(example=True)
-        lines = self.load_all_input_lines()
+        total_p1 = 0
+        for pos in x_pos:
+            for dx, dy in STANDARD_DIRECTIONAL_OFFSETS:
+                if self._check_direction(puzzle, pos, dx, dy):
+                    total_p1 += 1
 
-        return 'TODO'
+        total_p2 = 0
+        for pos in a_pos:
+            up_left = puzzle.get((pos[0] - 1, pos[1] - 1))
+            up_right = puzzle.get((pos[0] + 1, pos[1] - 1))
+            down_left = puzzle.get((pos[0] - 1, pos[1] + 1))
+            down_right = puzzle.get((pos[0] + 1, pos[1] + 1))
 
+            d1 = {up_left, down_right}
+            d2 = {up_right, down_left}
 
-Day04Solver().print_results()
+            if d1 == d2 == self.TARGET_P2:
+                total_p2 += 1
+
+        return total_p1, total_p2
+
+    def _check_direction(self, puzzle, pos, dx, dy):
+        cur_pos = pos
+
+        for val in 'MAS':
+            cur_pos = cur_pos[0] + dx, cur_pos[1] + dy
+            if puzzle.get(cur_pos) != val:
+                return False
+
+        return True
