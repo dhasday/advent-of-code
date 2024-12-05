cde6493089d9b44fae493dca2c14ca420e124740
Dan Hasday <dhasday@gmail.com>
2024-12-05 07:31:23 -0500

    2024 Solution for Day 5, prep for Day 6
     - Note to self - read the problem better next time

--- src/main/python/aoc/dec2024/day05.py
+++ src/main/python/aoc/dec2024/day05.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+from functools import cmp_to_key
+
 from aoc.common.day_solver import DaySolver
 
 
@@ -5,24 +8,33 @@ class Day05Solver(DaySolver):
     year = 2024
     day = 5
 
-    def setup(self):
-        pass
-
-    def solve_puzzle_one(self):
-        # line = self.load_only_input_line(example=True)
-        line = self.load_only_input_line()
-        # lines = self.load_all_input_lines(example=True)
+    def solve_puzzles(self):
         lines = self.load_all_input_lines()
 
-        return 'TODO'
+        split_idx = lines.index('')
+        rules = defaultdict(set)
+        for line in lines[0:split_idx]:
+            v1, v2 = line.split('|')
+            rules[v1].add(v2)
 
-    def solve_puzzle_two(self):
-        # line = self.load_only_input_line(example=True)
-        line = self.load_only_input_line()
-        # lines = self.load_all_input_lines(example=True)
-        lines = self.load_all_input_lines()
+        cmp = cmp_to_key(lambda x, y:  y not in rules[x])
+        total_p1 = 0
+        total_p2 = 0
+        for manual in lines[split_idx + 1:]:
+            pages = manual.split(',')
 
-        return 'TODO'
+            if self._is_valid(rules, pages):
+                total_p1 += int(pages[len(pages) // 2])
+            else:
+                reordered = sorted(pages, key=cmp)
+                total_p2 += int(reordered[len(reordered) // 2])
 
+        return total_p1, total_p2
 
-Day05Solver().print_results()
+    def _is_valid(self, rules, manual):
+        for idx, val in enumerate(manual):
+            before = rules[val]
+            if before.intersection(manual[:idx]):
+                return False
+
+        return True
