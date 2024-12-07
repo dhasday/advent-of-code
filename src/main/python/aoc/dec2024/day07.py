0f3dcf19d26c268cd492bd655b6aa53d2edf42d0
Dan Hasday <dhasday@gmail.com>
2024-12-07 00:53:49 -0500

    Solution for 2024 Day 7 and prep for Day 8

--- src/main/python/aoc/dec2024/day07.py
+++ src/main/python/aoc/dec2024/day07.py
@@ -5,24 +5,44 @@ class Day07Solver(DaySolver):
     year = 2024
     day = 7
 
-    def setup(self):
-        pass
-
-    def solve_puzzle_one(self):
-        # line = self.load_only_input_line(example=True)
-        line = self.load_only_input_line()
-        # lines = self.load_all_input_lines(example=True)
+    def solve_puzzles(self):
         lines = self.load_all_input_lines()
 
-        return 'TODO'
+        total_p1 = 0
+        total_p2 = 0
+        for line in lines:
+            target, values = line.split(': ')
 
-    def solve_puzzle_two(self):
-        # line = self.load_only_input_line(example=True)
-        line = self.load_only_input_line()
-        # lines = self.load_all_input_lines(example=True)
-        lines = self.load_all_input_lines()
+            target = int(target)
+            nums = [int(v) for v in values.split(' ')]
 
-        return 'TODO'
+            p1_valid, p2_valid = self._is_valid(target, nums)
+            if p1_valid:
+                total_p1 += target
+            if p2_valid:
+                total_p2 += target
 
+        return total_p1, total_p2
 
-Day07Solver().print_results()
+    def _is_valid(self, target, nums):
+        current_values = set()
+        current_values.add((nums[0], False))
+        for num in nums[1:]:
+            next_values = set()
+            for cur_val, used_concat in current_values:
+                next_add = cur_val + num
+                if next_add <= target:
+                    next_values.add((next_add, used_concat))
+
+                next_mult = cur_val * num
+                if next_mult <= target:
+                    next_values.add((next_mult, used_concat))
+
+                next_concat = int(str(cur_val) + str(num))
+                if next_concat <= target:
+                    next_values.add((next_concat, True))
+            current_values = next_values
+
+        p1_valid = (target, False) in current_values
+        p2_valid = p1_valid or (target, True) in current_values
+        return p1_valid, p2_valid
