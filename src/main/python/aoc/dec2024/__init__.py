0c7581fe03365fc2a4ae8e5301f5aef59a8252cc
Dan Hasday <dhasday@gmail.com>
2024-12-09 01:11:57 -0500

    Solution for 2024 Day 9, prep for day 10
     - Committing so I can get some sleep, but there's probably some optimizations I could do to this

--- src/main/python/aoc/dec2024/__init__.py
+++ src/main/python/aoc/dec2024/__init__.py
@@ -8,7 +8,7 @@ from .day06 import Day06Solver
 from .day07 import Day07Solver
 from .day08 import Day08Solver
 from .day09 import Day09Solver
-# from .day10 import Day10Solver
+from .day10 import Day10Solver
 
 # from .day11 import Day11Solver
 # from .day12 import Day12Solver
