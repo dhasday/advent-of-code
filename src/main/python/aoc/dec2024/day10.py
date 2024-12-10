996585ed58ee47a6d33d8877ddfe096bd0f8ddf2
Dan Hasday <dhasday@gmail.com>
2024-12-10 08:09:54 -0500

    Solution for 2024 Day 10, prep for Day 11
     - Ended up accidentally solving part 2 for part 1 because I misread the problem

--- src/main/python/aoc/dec2024/day10.py
+++ src/main/python/aoc/dec2024/day10.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+from aoc.common import helpers
 from aoc.common.day_solver import DaySolver
 
 
@@ -5,24 +8,26 @@ class Day10Solver(DaySolver):
     year = 2024
     day = 10
 
-    def setup(self):
-        pass
-
-    def solve_puzzle_one(self):
-        # line = self.load_only_input_line(example=True)
-        line = self.load_only_input_line()
-        # lines = self.load_all_input_lines(example=True)
+    def solve_puzzles(self):
         lines = self.load_all_input_lines()
 
-        return 'TODO'
+        height_to_pos = defaultdict(set)
 
-    def solve_puzzle_two(self):
-        # line = self.load_only_input_line(example=True)
-        line = self.load_only_input_line()
-        # lines = self.load_all_input_lines(example=True)
-        lines = self.load_all_input_lines()
+        for y, line in enumerate(lines):
+            for x, val in enumerate(line):
+                height_to_pos[int(val)].add((x, y))
 
-        return 'TODO'
+        available = [(v, v) for v in height_to_pos[0]]
+        for i in range(1, 10):
+            if not available:
+                break
 
+            next_available = []
+            for start_pos, last_pos in available:
+                for offset in helpers.STANDARD_DIRECTIONS:
+                    next_pos = last_pos[0] + offset[0], last_pos[1] + offset[1]
+                    if next_pos in height_to_pos[i]:
+                        next_available.append((start_pos, next_pos))
+            available = next_available
 
-Day10Solver().print_results()
+        return len(set(available)), len(available)
