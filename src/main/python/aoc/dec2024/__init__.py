0ce22136bbcdf68bdd39bfd8e6508c7bd2c0ffea
Dan Hasday <dhasday@gmail.com>
2024-12-14 01:38:03 -0500

    Solution for 2024 Day 14, prep for Day 15

--- src/main/python/aoc/dec2024/__init__.py
+++ src/main/python/aoc/dec2024/__init__.py
@@ -14,7 +14,7 @@ from .day11 import Day11Solver
 from .day12 import Day12Solver
 from .day13 import Day13Solver
 from .day14 import Day14Solver
-# from .day15 import Day15Solver
+from .day15 import Day15Solver
 
 # from .day16 import Day16Solver
 # from .day17 import Day17Solver
--- src/main/python/aoc/dec2024/day14.py
+++ src/main/python/aoc/dec2024/day14.py
@@ -1,3 +1,6 @@
+import math
+
+from aoc.common import helpers
 from aoc.common.day_solver import DaySolver
 
 
@@ -5,24 +8,46 @@ class Day14Solver(DaySolver):
     year = 2024
     day = 14
 
-    def setup(self):
-        pass
-
-    def solve_puzzle_one(self):
-        # line = self.load_only_input_line(example=True)
-        line = self.load_only_input_line()
-        # lines = self.load_all_input_lines(example=True)
+    def solve_puzzles(self):
         lines = self.load_all_input_lines()
 
-        return 'TODO'
+        size_x = 101
+        size_y = 103
+        mid_x = size_x // 2
+        mid_y = size_y // 2
 
-    def solve_puzzle_two(self):
-        # line = self.load_only_input_line(example=True)
-        line = self.load_only_input_line()
-        # lines = self.load_all_input_lines(example=True)
-        lines = self.load_all_input_lines()
+        robots = []
+        for line in lines:
+            values = helpers.parse_all_numbers(line)
+            pos = values[0], values[1]
+            vel = values[2], values[3]
 
-        return 'TODO'
+            robots.append((pos, vel))
 
+        ans_one = None
+        min_safety_factor = math.inf
+        min_time = None
+        for i in range(10_000):
+            quadrants = [0, 0, 0, 0]
+            for pos, vel in robots:
+                cur_pos = (pos[0] + (vel[0] * i)) % size_x, (pos[1] + (vel[1] * i)) % size_y
+                if cur_pos[0] < mid_x:
+                    if cur_pos[1] < mid_y:
+                        quadrants[0] += 1
+                    elif cur_pos[1] > mid_y:
+                        quadrants[1] += 1
+                elif cur_pos[0] > mid_x:
+                    if cur_pos[1] < mid_y:
+                        quadrants[2] += 1
+                    elif cur_pos[1] > mid_y:
+                        quadrants[3] += 1
+            safety_factor = math.prod(quadrants)
 
-Day14Solver().print_results()
+            if safety_factor != 0 and safety_factor < min_safety_factor:
+                min_safety_factor = safety_factor
+                min_time = i
+
+            if i == 100:
+                ans_one = safety_factor
+
+        return ans_one, min_time
