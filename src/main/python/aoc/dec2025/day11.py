31a501fc69cee9da16bdad3ad5ee27df6f637a5f
Dan Hasday <dhasday@gmail.com>
2025-12-11 01:09:43 -0500

    Solution for 2025 day 11

--- src/main/python/aoc/dec2025/day11.py
+++ src/main/python/aoc/dec2025/day11.py
@@ -1,3 +1,5 @@
+from functools import cache
+
 from aoc.common.day_solver import DaySolver
 
 
@@ -5,24 +7,32 @@ class Day11Solver(DaySolver):
     year = 2025
     day = 11
 
+    devices = None
+
     def setup(self):
-        pass
+        lines = self.load_all_input_lines()
+
+        self.devices = dict()
+        for line in lines:
+            device, rest = line.split(":")
+            self.devices[device] = set(rest.strip().split(" "))
 
     def solve_puzzle_one(self):
-        # line = self.load_only_input_line(example=True)
-        line = self.load_only_input_line()
-        # lines = self.load_all_input_lines(example=True)
-        lines = self.load_all_input_lines()
-
-        return 'TODO'
+        return self._find_path_count("you")
 
     def solve_puzzle_two(self):
-        # line = self.load_only_input_line(example=True)
-        line = self.load_only_input_line()
-        # lines = self.load_all_input_lines(example=True)
-        lines = self.load_all_input_lines()
+        return self._find_path_count("svr", required=("fft", "dac"))
 
-        return 'TODO'
+    @cache
+    def _find_path_count(self, current, target="out", required=()):
+        if current == target:
+            # If there are any required items left, this path doesn't count
+            return 1 if not required else 0
 
+        if current in required:
+            required = tuple(v for v in required if v != current)
 
-Day11Solver().print_results()
+        return sum(
+            self._find_path_count(device, target, required)
+            for device in self.devices[current]
+        )
